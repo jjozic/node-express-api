48ed7a12ad0c215e6e10485253c797f781ad374b
Julian Jozic <jjozic98@gmail.com>
2020-07-24 13:38:21 +0200

    Added usersRoutes to the project
    - so we can create/read users

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,25 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/users', (req, res) => {
-    res.send('Hello')
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    }
+]
+
+// all routes in here start with /users
+router.get('/', (req, res) => {
+    res.send(users)
 })
+
+router.post('/', (req, res) => {
+    const user = req.body
+
+    users.push(user)
+
+    res.send('added user')
+})
+
+export default router
